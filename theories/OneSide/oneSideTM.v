d3e30fe98f6f9d946d7f84d76aa261147989764e
Marcel Ullrich <ullrich.marcel@web.de>
2020-09-07 23:03:34 +0200

    transformation

--- theories/OneSide/oneSideTM.v
+++ theories/OneSide/oneSideTM.v
@@ -81,7 +81,6 @@ we are on the right extremity of a non-empty tape (right overflow). *)
   (*     end. *)
 
   (* makes a tape from left, current, right *)
-  Check singleTM.mk_tape.
    Definition mk_tape (ls:list sig) (c:option sig) (rs:list sig) : tape :=
      mkTape (|ls|) (fun n => nth_error (singleTM.tapeToList(singleTM.mk_tape ls c rs)) n).
     (* refine (mkTape (|ls|) ()).
@@ -250,3 +249,222 @@ Definition single_TM_halt : { sig : finType & (TM sig 1) * (tapes sig 1)}%type -
   { f_config (TM_facts.mk_mconfig a [|b|]) := mk_mconfig a b }. *)
 
 (* From Undecidability Require Import TM.Util.Prelim TM.Util.TM_facts. *)
+
+
+Lemma countMap {X Y:eqType} (f:X->Y) (xs:list X) (x:X):
+  injective f ->
+  BasicDefinitions.count xs x = BasicDefinitions.count (map f xs) (f x).
+Proof.
+  move => H.
+  elim: xs => //=.
+  move => y ys IH.
+  case: (Dec (x = y));
+  case: (Dec (f x = f y)) => //=.
+  2: move => + H2; by rewrite H2.
+  2: by move => /H ->.
+  move => _ _. by f_equal.
+Qed.
+
+Section Reduction.
+
+
+  Variable (sig:finType).
+
+  Section SymbolTranform.
+
+    Inductive sigRev := Symbol (s:sig) | Reverse.
+
+    (* Instance sigRev_eq_dec : eq_dec sigRev.
+    Proof.
+      unfold EqDec.dec. 
+      decide equality.
+      eapply eqType_dec.
+    Defined.
+    Canonical Structure EqSigRev := EqType sigRev. *)
+
+    Global Instance sigRev_eq_dec : eq_dec sigRev.
+    Proof.
+      intros. hnf. decide equality. apply eqType_dec.
+    Defined.
+
+
+    Global Instance sigRev_finC : finTypeC (EqType sigRev).
+    Proof.
+      apply (FinTypeC (enum := Reverse::map Symbol (let (e,_) := (class sig) in e))).
+      intros [];simpl.
+      - case: (class sig) => enum /(_ s) H.
+        rewrite <- countMap;first done.
+        move => x y {}H. by injection H.
+      - case: (class sig) => + _.
+        elim => //=.
+    Qed.
+
+    Definition sigRev_finType : finType := (@FinType (EqType sigRev) sigRev_finC).
+
+  End SymbolTranform.
+
+  Section TMTransform.
+
+    Variable tm : sTM sig.
+    Definition tmStates := singleTM.states tm.
+    Definition tmTrans := @singleTM.trans _ tm.
+    Definition tmStart := singleTM.start tm.
+    Definition tmHalt := @singleTM.halt _ tm.
+
+    Inductive osStates := 
+      newStart | 
+      sState (s:tmStates) | 
+
+      l2State (s:tmStates) | 
+      n2State (s:tmStates) | 
+      r2State (s:tmStates) | 
+
+      r1State (s:tmStates) | 
+      n1State (s:tmStates).
+
+    Global Instance osStates_eq_dec : eq_dec osStates.
+    Proof.
+      intros. hnf. decide equality; apply eqType_dec.
+    Defined.
+
+    Global Instance osStates_finC : finTypeC (EqType osStates).
+    Proof.
+      apply (FinTypeC (enum := 
+        [newStart]++
+        map sState (elem tmStates) ++
+        map l2State (elem tmStates) ++
+        map n2State (elem tmStates) ++
+        map r2State (elem tmStates) ++
+        map r1State (elem tmStates) ++
+        map n1State (elem tmStates)
+      )).
+      intros [];simpl.
+      1: {
+        elim (elem tmStates) => //= y ys.
+        by do 10 (rewrite -countSplit;simpl).
+      }
+      all: unfold elem;
+        do 5 (rewrite -countSplit;simpl);
+        rewrite -countMap;first by (move => x y {}H; injection H).
+      all: rewrite enum_ok;by elim enum.
+    Qed.
+
+    Definition osStates_finType : finType := (@FinType (EqType osStates) osStates_finC).
+
+    Definition osHalt (p:osStates) :=
+      match p with
+      | sState q => tmHalt q
+      | _ => false
+      end.
+
+    Definition osStart := newStart.
+
+    Definition embedOption {X Y:Type} (f:X->Y) (a:option X) : option Y :=
+      match a with 
+      | None => None
+      | Some x => Some (f x)
+      end.
+
+    Definition partStep (p:tmStates) (a:option sig) : osStates * (option sigRev * move) :=
+      match tmTrans (p,a) with
+      | (q, (b, Rmove)) => 
+          (r2State q, (embedOption Symbol b, Rmove))
+      |  (q, (b, Lmove)) => 
+          (l2State q, (embedOption Symbol b, Lmove))
+      |  (q, (b, Nmove)) => 
+          (n2State q, (embedOption Symbol b, Nmove))
+      end.
+
+    Definition osTrans : osStates * option sigRev -> osStates * (option sigRev * move) :=
+      fun '(p,s) =>
+      match p,s with
+      | newStart, _ => (sState tmStart, (Some Reverse, Rmove)) 
+        (* in Reality: start on black writes Reverse and begins TM *)
+
+      | r1State q, y => (sState q, (y, Rmove))
+      | n1State q, y => (sState q, (y, Nmove))
+
+      | r2State q, x => (n1State q, (x, Rmove))
+      | l2State q, Some Reverse => (r1State q, (Some Reverse, Rmove))
+      | l2State q, x => (n1State q, (x, Lmove))
+
+      (* normal *)
+      | sState p, None => partStep p None
+      | sState p, Some (Symbol a) => partStep p (Some a)
+      (* s is None / Some (Symbol a)   not Some Reverse *)
+
+      | _,_ => (newStart,(None,Nmove))
+      end.
+
+      Definition ostm : osTM sigRev_finType :=
+        {|
+          states := osStates_finType;
+          trans := osTrans;
+          start := osStart;
+          halt := osHalt
+        |}.
+
+  End TMTransform.
+
+  Section TapeTransform.
+
+    (* TM.tape sig -> tape sig *)
+
+    Print tape.
+    Print TM.tape.
+
+    (* idea: 
+      new tape: reverse 0 -1 1 -2 2 -3 ...
+      reverse [n;-(S n)]
+     *)
+
+  End TapeTransform.
+  
+End Reduction.
+
+Lemma TM_conv: singleTM.Halt ⪯ Halt.
+Proof.
+  unshelve eexists.
+
+(* Lemma TM_conv : HaltTM 1 ⪯ Halt.
+Proof.
+  unshelve eexists.
+  - intros [sig [Q trans0 start0 halt0] t]. do 2 depelim t.
+    exists sig. econstructor.
+    + intros []. destruct (trans0 (e, [| o |])) eqn:E.
+      do 2 depelim t.
+      exact (e0, h0).
+    + exact start0.
+    + exact halt0.
+    + exact h.
+  - intros [sig [Q trans0 start0 halt0] t]. cbn. do 2 depelim t; cbn.
+    unfold Halt. rewrite <- TM_terminates_Halt. cbn.
+    unfold HaltsTM at 1.
+    setoid_rewrite TM_eval_iff.
+    split.
+    + intros (cstate & ctapes & x0 & H). 
+      do 2 depelim ctapes.
+      eexists x0, (singleTM.mk_mconfig cstate h0). cbn in *. 
+      unfold loopM, TM_facts.loopM in *.
+      eapply loop_lift with (lift := @f_config _ _) in H.
+      cbn in H. rewrite <- H.
+      f_equal.
+      * reflexivity.
+      * intros. cbn. depelim x. depelim ctapes. depelim ctapes. reflexivity.
+      * intros. cbn. depelim x. depelim ctapes. depelim ctapes. cbn in *.
+        unfold singleTM.step, TM_facts.step. cbn.
+        unfold current_chars. cbn.
+        destruct (trans0 (cstate0, [|current h1|])) eqn:E. 
+        depelim t. depelim t. cbn. reflexivity.        
+    + intros (? & ? & ?).
+      cbn in *. destruct x0.
+      exists cstate, [| ctape0 |], x.
+      unfold TM_facts.loopM, singleTM.loopM in *.
+      eapply (loop_lift (lift := fun '(singleTM.mk_mconfig a b) => @TM_facts.mk_mconfig sig Q 1 a [| b |])) in H. cbn.
+      rewrite <- H.
+      f_equal.
+      * now intros [].
+      * intros [] ?. unfold singleTM.step, TM_facts.step, current_chars.
+        cbn. destruct (trans0 (cstate0, [|current ctape1|])) eqn:E.
+        depelim t. depelim t. reflexivity.
+Qed.  *)
